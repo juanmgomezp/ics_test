ddb8c99713c79ace3899b1f6c24d35d3e050b686
juanmgomezp <juanmgomezpalma@gmail.com>
2021-10-19 11:48:54 -0500

    otra prueba

--- src/routes/calendar.routes.js
+++ src/routes/calendar.routes.js
@@ -3,10 +3,10 @@ const Router = require('express').Router;
 const CalendarRoute = Router();
 
 const eol = "\r\n";
-const calendar = `
-BEGIN:VCALENDAR\r\n
-VERSION:2.0\r\n
-PRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\n
+const content = `
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
 METHOD:PUBLISH
 BEGIN:VEVENT
 UID:20211015T172345Z-AF23B2@nasa.com
@@ -25,7 +25,7 @@ END:VEVENT
 END:VCALENDAR
 `;
 
-const content = calendar.replace(["\r\n", "\r", "\n"], "\r\n");
+//const content = calendar.replace(["\r\n", "\r", "\n"], "\r\n");
 console.log(content);
 CalendarRoute.get('/calendar/calendar.ics', (req, res) => {
     res.set({
