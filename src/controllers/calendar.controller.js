de668d6930ab6cd946baf1c21a6c7832e12c5e4a
juanmgomezp <juanmgomezpalma@gmail.com>
2021-10-19 19:58:18 -0500

    añadiendo calendar.controller.js

---
+++ src/controllers/calendar.controller.js
@@ -0,0 +1,54 @@
+const Rooms = require('../models/rooms');
+const Reservations = require('../models/reservations');
+const ical = require('ical-generator');
+
+const generateCalendar = async (req, res) => {
+    try {
+        const roomId = req.params.roomId;
+        const roomFounded = await Rooms.findById(roomId)
+    
+        if(!roomFounded) return res.status(500).json({msg: 'invalid url'});    
+
+        const reservations = await Reservations.find({room: roomId});
+        
+        let events = [];
+
+        if(reservations.length > 0){
+            reservations.map(item => {
+                let event = {
+                    start: new Date(item.start),
+                    end: new Date(item.end),
+                    id: item.id,
+                    summary: item.guest,
+                    description: `Reservation for ${item.guest}`,
+                    ur: 'https://www.otl.io'                    
+                }
+                events.push(event);
+            });
+        }
+
+        const cal = ical({
+            prodId: `//${roomId}//otl.io//EN`, 
+            method: 'PUBLISH',
+            name: `${roomFounded.name}`,
+            events: events
+        });
+        
+        res.set({
+            'content-type':'text/calendar; method=PUBLISH; charset=utf-8',
+            'Mime-Version':'1.0',
+            'Content-Transfer-Encoding': 'quoted-printable',
+            'content-disposition': 'inline; filename=calendar.ics',
+            'x-content-type-options': 'nosniff',
+            'x-xss-protection': '0',
+            'x-frame-options': 'SAMEORIGIN'
+        });
+        
+        cal.serve(res);
+        
+    } catch (error) {
+       console.log(error); 
+    }
+}
+
+module.exports = generateCalendar;
